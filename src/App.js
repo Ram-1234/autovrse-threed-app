e8257baa130af2dd410175b828f800e78269c444
Ram-1234 <ramnayan699@gmail.com>
2021-05-25 07:37:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React,{useRef,useState} from 'react'
+import { Canvas , useFrame} from 'react-three-fiber'
+import {useSpring,a} from "@react-spring/three"
+//import { Box } from '@react-three/drei';
+
 import './App.css';
 
-function App() {
+
+const Box=({position,color})=>{
+  const mesh=useRef(null);
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  const [expand, setExpand]=useState(false);
+  const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
+         <boxBufferGeometry attach="geometry" args={[2,2,2]} />
+         <meshStandardMaterial attach="material" color={color} />
+       </a.mesh>     
   );
 }
+
+const Sphere=({position, color})=>{
+  const mesh=useRef(null);
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  const [expand, setExpand]=useState(false);
+  const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
+  return (
+       <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
+         <sphereBufferGeometry attach="geometry" args={[2,10,15]} />
+         <meshStandardMaterial attach="material" color={color} />
+       </a.mesh>     
+  );
+}
+
+const Cylinder=({position,color})=>{
+  const mesh=useRef(null);
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  const [expand, setExpand]=useState(false);
+  const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
+  return (
+    <>
+       <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
+         <cylinderBufferGeometry attach="geometry" args={[1,1,4,10]} />
+         <meshStandardMaterial attach="material" color={color} />
+       </a.mesh>   
+    </>  
+  );
+}
+
+function App() {
+  return(
+    <>
+     <Canvas  
+     colorManagement 
+     camera={{position:[-5,2,10], few:40}} >
+       <ambientLight  intensity={0.4} />
+       <directionalLight  
+        
+        position={[0,10,0]}
+        intensity={1.4}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={10}
+        shadow-camera-right={10}
+        shadow-camera-top={-10}
+        shadow-camera-bottom={-10}
+       />
+       <pointLight position={[-10,0,-15]} intensity={0.5} />
+       <pointLight position={[0,-10,0]} intensity={1.2} />
+      <group>
+        <mesh 
+        
+        rotation={[-Math.PI/2,0,0]} 
+        position={[0,-3,0]}>
+          <planeBufferGeometry attach='geometry' args={[100,100]} />
+          <shadowMaterial attach="material"  />
+        </mesh>
+      </group>
+
+       <Box position={[-4,1,-5]} color="#ef476f" />
+       <Cylinder position={[-1,1,0]} color="#4c956c" />
+       <Sphere position={[4,1,1]} color="#ff87ab" />
+     </Canvas>
+    </>
+  )
+  
+};
+
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import { Canvas } from 'react-three-fiber';
+// import './styles.css'
+//import {Canvas, useFrame} from "react-three-fiber";
+// import {softShadows, MeshWobbleMaterial,OrbitControls} from 'drei';
+//import {useSpring, a} from "react-spring/three";
+
+//softShadows();
+
+
+// export default function Header() {
+//     return (
+//         <>
+//             <Canvas>
+//                 <mesh>
+//                     <boxBufferGeometry attach="geometry" args={[1,1,1]}/>
+//                     <meshStandardMaterial attach='material' />
+//                 </mesh>
+//             </Canvas>
+//         </>
+//     )
+// }
