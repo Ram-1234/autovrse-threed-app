decec32e2919222e5b2c43ed3f0becbe1182d48c
Ram-1234 <ramnayan699@gmail.com>
2021-05-26 10:44:39 +0530

    modidied

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import './App.css';
 
 const Box=({position,color})=>{
   const mesh=useRef(null);
-  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.01));
   const [expand, setExpand]=useState(false);
   const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
   return (
        <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
-         <boxBufferGeometry attach="geometry" args={[2,2,2]} />
+         <boxBufferGeometry attach="geometry" args={[3,3,3]} />
          <meshStandardMaterial attach="material" color={color} />
        </a.mesh>     
   );
@@ -21,12 +21,12 @@ const Box=({position,color})=>{
 
 const Sphere=({position, color})=>{
   const mesh=useRef(null);
-  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.01));
   const [expand, setExpand]=useState(false);
   const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
   return (
        <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
-         <sphereBufferGeometry attach="geometry" args={[2,10,15]} />
+         <sphereBufferGeometry attach="geometry" args={[2,10,100]} />
          <meshStandardMaterial attach="material" color={color} />
        </a.mesh>     
   );
@@ -34,13 +34,13 @@ const Sphere=({position, color})=>{
 
 const Cylinder=({position,color})=>{
   const mesh=useRef(null);
-  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.015));
+  useFrame(()=>(mesh.current.rotation.z = mesh.current.rotation.x = mesh.current.rotation.y +=0.01));
   const [expand, setExpand]=useState(false);
   const props=useSpring({scale: expand ? [1.4,1.4,1.4]:[1,1,1],})
   return (
     <>
        <a.mesh onClick={()=>setExpand(!expand)} scale={props.scale} position={position} ref={mesh}>
-         <cylinderBufferGeometry attach="geometry" args={[1,1,4,10]} />
+         <cylinderBufferGeometry attach="geometry" args={[1,1,4,50,true]} />
          <meshStandardMaterial attach="material" color={color} />
        </a.mesh>   
     </>  
@@ -52,7 +52,7 @@ function App() {
     <>
      <Canvas  
      colorManagement 
-     camera={{position:[-5,2,10], few:40}} >
+     camera={{position:[-5,2,10], few:60}} >
        <ambientLight  intensity={0.4} />
        <directionalLight  
         
@@ -80,7 +80,7 @@ function App() {
 
        <Box position={[-4,1,-5]} color="#ef476f" />
        <Cylinder position={[-1,1,0]} color="#4c956c" />
-       <Sphere position={[4,1,1]} color="#ff87ab" />
+       <Sphere position={[4,1,1]} color="#d00000" />
      </Canvas>
     </>
   )
